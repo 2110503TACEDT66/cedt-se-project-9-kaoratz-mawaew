63de0c1c3781ac6aaf318fbf9a980e33eee10b85
Gannub <gannubvv@gmail.com>
2024-04-10 00:04:32 +0700

    mod gitignore, server.js, local envs

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const ratelimit = require('express-rate-limit');
 const hpp = require('hpp');
 const cors = require('cors');
 
+//Load env vars
+dotenv.config({path : 'config/config.env'});
+
 const restaurant = require('./routes/restaurants');
 const auth = require('./routes/auth');
 const reservation  = require('./routes/reservations');
@@ -19,8 +22,6 @@ const limiter = ratelimit({
     max: 1000
 });
 
-//Load env vars
-dotenv.config({path : 'config/config.env'});
 
 //connect to db
 connectDB();
@@ -35,6 +36,7 @@ app.use(xss());
 app.use(limiter);
 app.use(hpp());
 app.use(cors());
+
 app.use('/api/v1/restaurants', restaurant);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/reservations', reservation );
@@ -45,7 +47,7 @@ app.use('/api/v1/payments',payments);
 
 //Set static folder
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('server running in ',process.env.HOST, ':', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'on ' + process.env.HOST + ":" + PORT));
 
 //handle unhandled promise rejection
 process.on('unhandledRejection', (err,promise) => {
