1ad6e05b6db700b7897331348b31655936e2b3dd
BuildTools <unconfigured@null.spigotmc.org>
2024-04-10 21:27:28 +0700

    add tag to schema and delete project that in frontend

--- frontend/swdevprac2-project-i-cut-my-hair-main/src/libs/getRestaurant.tsx
+++
@@ -1,9 +0,0 @@
-
-export default async function getRestaurant(id:string) {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/restaurants/${id}`)
-
-    if(!response){
-        throw new Error(`Failed to fetch restaurant with id of ${id}`)
-    }
-    return await response.json()
-}
