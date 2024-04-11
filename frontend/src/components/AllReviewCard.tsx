6f0b15429de2d42bb108b422bde56812c2a16209
Auiz <nopphawit47374@gmail.com>
2024-04-11 15:09:13 +0700

    update globals.css

--- frontend/src/components/AllReviewCard.tsx
+++ frontend/src/components/AllReviewCard.tsx
@@ -2,7 +2,7 @@ import ReviewCard from "./ReviewCard";
 
 export default function AllReviewCard() {
     return (
-        <div className="m-5 flex flex-row overflow-x-scroll">
+        <div id="all-review-card" className="m-5 flex flex-row overflow-x-scroll">
             <ReviewCard/>
             <ReviewCard/>
             <ReviewCard/>
