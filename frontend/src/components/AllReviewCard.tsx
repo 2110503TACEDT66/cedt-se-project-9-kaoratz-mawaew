4880f7e4fb61f97325b5d00bad666d5787fc0a1e
P-3142857 <144462891+P-3142857@users.noreply.github.com>
2024-04-11 17:13:47 +0900

    Edit review frontend

--- frontend/src/components/AllReviewCard.tsx
+++ frontend/src/components/AllReviewCard.tsx
@@ -1,12 +1,11 @@
+import getReviews from "@/libs/getReviews";
 import ReviewCard from "./ReviewCard";
 
 export default function AllReviewCard() {
+    const reviews = getReviews();
     return (
         <div className="m-5 flex flex-row overflow-x-scroll">
-            <ReviewCard/>
-            <ReviewCard/>
-            <ReviewCard/>
-            <ReviewCard/>
+            <ReviewCard ReviewJson={reviews}></ReviewCard>
         </div>
     )
 }
--- frontend/src/components/ReviewCard.tsx
+++ frontend/src/components/ReviewCard.tsx
@@ -1,21 +1,17 @@
 import { Rating, RatingStar } from "flowbite-react"
+import { ReviewJson, ReviewItem } from "../../interface"
 
-export default function ReviewCard() {
+export default async function ReviewCard({ReviewJson}:{ReviewJson:Promise<ReviewJson>}) {
+    const reviewReady = await ReviewJson;
     return (
         <div className="w-2/5 m-10 rounded-xl shadow-[-1px_1px_3px_rgba(0,0,0,0.3)] flex-none">
             <div className="bg-[#ededed] rounded-xl shadow-[4px_6px_6px_rgba(0,0,0,0.3)]">
                 <div className="border-[7px] border-[#ededed] rounded-xl p-5 shadow-[inset_6px_6px_12px_rgba(0,0,0,0.3)]">
-                    <h1 className="font-sans text-xl font-bold">Name Surname</h1>
-                    <Rating>
-                        <RatingStar className="h-6 w-6" />
-                        <RatingStar className="h-6 w-6" />
-                        <RatingStar className="h-6 w-6" />
-                        <RatingStar className="h-6 w-6 text-[#ffffff]" />
-                        <RatingStar className="h-6 w-6 text-[#ffffff]" />
-                        &nbsp;2 month ago
-                    </Rating>
-                    <br/>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum veniam suscipit totam et ratione repellat consectetur, dolor, pariatur doloribus minima provident adipisci, iure dolore. Ullam dolorum amet molestias sit error.<br/>
+                    reviewReady.data? 
+                        reviewReady.data.map((reviewItems: ReviewItem) =>
+                        <h1 className="font-sans text-xl font-bold"> </h1>
+                        ) : null
+
                 </div>
             </div>
         </div>
