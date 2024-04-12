67ccf011c969cfe594be9c75311af8c3254e3c81
Auiz <nopphawit47374@gmail.com>
2024-04-12 23:10:48 +0700

    edit status

--- frontend/src/components/ridpage/RestaurantTime.tsx
+++ frontend/src/components/ridpage/RestaurantTime.tsx
@@ -1,13 +1,19 @@
 import { RestaurantItem } from "../../../interface"
 
-export default function RestaurantTime({restaurantDetails}: {restaurantDetails: RestaurantItem}) {
+export default function RestaurantTime({restaurantDetails, flag}: {restaurantDetails: RestaurantItem, flag: boolean}) {
     return (
         <div className="mb-5 shadow-[0_3px_3px_rgba(0,0,0,0.3)]">
             <div className="border-2 border-black px-6 py-2 bg-[#b1b3aa] shadow-[-3px_-3px_rgba(0,0,0,0.3)]">
                 <table className="w-full text-4xl mb-2">
                     <tr>
                         <td>Mon-Fri</td>
-                        <td className="text-right text-red-800 font-bold">CLOSED</td>
+                        <td className="text-right">
+                            {
+                                flag ? 
+                                    <p className="text-4xl text-green-600 font-bold">OPENED</p>:
+                                    <p className="text-4xl text-red-800 font-bold">CLOSED</p>
+                            }
+                        </td>
                     </tr>
                 </table>
                 <p className="w-full text-center text-[76px]">{`${restaurantDetails.opentime} - ${restaurantDetails.closetime}`}</p>
