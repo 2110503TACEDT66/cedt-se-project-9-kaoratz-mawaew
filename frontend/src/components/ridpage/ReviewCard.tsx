c932435e75baa42f550e5cd9f2b68622839a9174
Auiz <nopphawit47374@gmail.com>
2024-04-17 12:53:55 +0700

    change layout of ridpage

--- frontend/src/components/ridpage/ReviewCard.tsx
+++ frontend/src/components/ridpage/ReviewCard.tsx
@@ -4,7 +4,7 @@ import { ReviewJson, ReviewItem } from "../../../interface"
 export default async function ReviewCard({ReviewItem}:{ReviewItem:ReviewItem}) {
     return (
         <>
-        <div className="w-2/5 h-[15%] m-4 rounded-xl shadow-[-1px_1px_3px_rgba(0,0,0,0.3)] flex-none font-mono">
+        <div className="w-2/5 h-[15%] m-4 rounded-xl shadow-[-1px_1px_3px_rgba(0,0,0,0.3)] flex-none font-mono mb-10">
             <div className="bg-[#ededed] rounded-xl shadow-[4px_6px_6px_rgba(0,0,0,0.3)]">
                 <div className="border-[7px] border-[#ededed] rounded-xl p-5 shadow-[inset_6px_6px_12px_rgba(0,0,0,0.3)]">
 
