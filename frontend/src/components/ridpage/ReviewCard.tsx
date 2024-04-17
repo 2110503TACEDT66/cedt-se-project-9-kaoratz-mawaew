8713eacb9429180b00a62e435925a6acd663a8cc
Auiz <nopphawit47374@gmail.com>
2024-04-17 13:15:29 +0700

    set rating color black

--- frontend/src/components/ridpage/ReviewCard.tsx
+++ frontend/src/components/ridpage/ReviewCard.tsx
@@ -9,7 +9,7 @@ export default async function ReviewCard({ReviewItem}:{ReviewItem:ReviewItem}) {
                 <div className="border-[7px] border-[#ededed] rounded-xl p-5 shadow-[inset_6px_6px_12px_rgba(0,0,0,0.3)]">
 
                     <h1 className="text-base font-bold text-[20px]">{ReviewItem.user.name} </h1>
-                    <Rating name="stars" defaultValue={ReviewItem.rating} readOnly size="small"/>
+                    <Rating name="stars" defaultValue={ReviewItem.rating} readOnly size="small" style={{color: 'black'}}/>
                     <p className="text-[14px] text-primary">{ReviewItem.comment}</p>
 
                 </div>
