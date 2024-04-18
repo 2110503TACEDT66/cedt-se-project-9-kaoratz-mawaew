211736455862fb219701e2b53c91a59ab7243180
Auiz <nopphawit47374@gmail.com>
2024-04-18 15:25:34 +0700

    edit restaurant name style

--- frontend/src/app/(restaurantInfo)/restaurant/[rid]/page.tsx
+++ frontend/src/app/(restaurantInfo)/restaurant/[rid]/page.tsx
@@ -26,13 +26,11 @@ export default async function GetOne({ params }: { params: { rid: string } }) {
     // Compare current time with open and close times
     const flag = currentTime >= openTime && currentTime <= closeTime;
 
-    const mapHref = restaurantDetails.data.map ? restaurantDetails.data.map : '/';
-
     return (
         <div className="w-[83%] pl-4 font-mono ">
 
             <div className='flex justify-start'>
-                <h1 className="text-4xl font-bold mb-12 text-primary">{restaurantDetails.data.name}</h1>
+                <h1 className="text-4xl font-bold mb-12 text-primary text-nowrap">{restaurantDetails.data.name}</h1>
                 <hr className='border-black border-1 w-full m-5'/>
             </div>
             
