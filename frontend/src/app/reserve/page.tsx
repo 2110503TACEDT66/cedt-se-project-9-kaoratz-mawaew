b1f31a3572f0b53f6b76448f1281b3bd66f26030
kaorat <ratchapongolf9@gmail.com>
2024-04-19 01:09:37 +0700

    son will do reserve page and fix it

--- frontend/src/app/reserve/page.tsx
+++ frontend/src/app/reserve/page.tsx
@@ -18,7 +18,7 @@ export default function booking() {
     const urlParams = useSearchParams()
     const rid = urlParams.get('id')
     const { data: session } = useSession()
-    
+
     const [restaurantData, setRestaurantData] = useState<RestaurantItem | null>(null);
 
     interface RestaurantJsonHa {
@@ -26,34 +26,29 @@ export default function booking() {
         count: number,
         pagination: Object,
         data: RestaurantItem
-      }
+    }
 
-    const getRestaurantFunction = async (rid:string) => {
-            const restaurantJson : RestaurantJsonHa = await getRestaurant(rid);
-            let hahaha: RestaurantItem = restaurantJson.data;
-            setRestaurantData(hahaha)
-            if (hahaha) {
-                console.log(hahaha.name)
-            } else {
-                console.log("sdlfjka")
-            }
-        }
-    
+    const getRestaurantFunction = async (rid: string) => {
+        const restaurantJson: RestaurantJsonHa = await getRestaurant(rid);
+        let hahaha: RestaurantItem = restaurantJson.data;
+        setRestaurantData(hahaha)
+    }
+
     useEffect(() => {
-    
+
 
         if (rid) {
             getRestaurantFunction(rid);
         }
 
         // Clean up interval on component unmount
-        
+
     }, []);
 
     const [bookDate, setBookDate] = useState<Date | null>(dayjs().toDate())
     const [numberValue, setnumberValue] = useState<number>(0)
-    
-    
+
+
     return (
         <main className="flex flex-col w-[70%] pl-4">
 
@@ -63,42 +58,42 @@ export default function booking() {
                     <div>
                         <p className="text-4xl mb-16 font-mono">Reserve Table</p>
                         {
-                
-                            restaurantData?
-                            <div className="flex flex-row w-full">
-                                <div>
-                                    <h1 className="text-4xl font-mono mb-12 text-primary text-nowrap">{restaurantData.name}</h1>
 
-                                    <Map restaurant={restaurantData}/>
-                                    
-                                    <div className="p-4">
-                                        {restaurantData.name}
-                                        <p>Address: {restaurantData.address}</p>
-                                        <p>Subdistrict: {restaurantData.subdistrict}</p>
-                                        <p>District {restaurantData.district}</p>
-                                        <p>Province: {restaurantData.province}</p>
-                                        <p>Postal Code: {restaurantData.postalcode}</p>
-                                        <p>Tel: {restaurantData.tel}</p>
+                            restaurantData ?
+                                <div className="flex flex-row w-full">
+                                    <div>
+                                        <h1 className="text-4xl font-mono mb-12 text-primary text-nowrap">{restaurantData.name}</h1>
+
+                                        <Map restaurant={restaurantData} />
+
+                                        <div className="p-4">
+                                            {restaurantData.name}
+                                            <p>Address: {restaurantData.address}</p>
+                                            <p>Subdistrict: {restaurantData.subdistrict}</p>
+                                            <p>District {restaurantData.district}</p>
+                                            <p>Province: {restaurantData.province}</p>
+                                            <p>Postal Code: {restaurantData.postalcode}</p>
+                                            <p>Tel: {restaurantData.tel}</p>
+                                        </div>
+
                                     </div>
-                                
-                                </div>                            
-                                <img src={restaurantData.imageUrl} alt="" className="w-full ml-9" />
-                            </div>
-                            : <p>Not found</p>
+                                    <img src={restaurantData.imageUrl} alt="" className="w-full ml-9" />
+                                </div>
+                                : <div><p>Not found</p></div>
                         }
-                        
-                        
+
+
                         <p className="font-mono">Date&Time</p>
-                        <p className="text-4xl mb-4 inline-block border border-stone-800 p-2">
-                            <DateReserve onDateTimeChange={(value:Date)=>{setBookDate(value)}}/>
-                        </p>
-                        
+                        <div className="text-4xl mb-4 inline-block border border-stone-800 p-2">
+                            <DateReserve onDateTimeChange={(value: Date) => { setBookDate(value) }} />
+                        </div>
+
                     </div>
                 </div>
 
             </div>
 
-            
+
             <div className="flex flex-row">
                 <Link href="/restaurant" className='w-[20%] mr-4 inline-block'>
                     <button className="text-base w-[100%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center relative overflow-hidden transition-transform duration-300 ease-in-out 
@@ -106,16 +101,16 @@ export default function booking() {
                         hover:-translate-x-1 hover:-translate-y-1">Back</button>
                 </Link>
 
-                { session && rid ? 
-                <button className="text-base w-[80%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center relative overflow-hidden transition-transform duration-300 ease-in-out 
+                {session && rid ?
+                    <button className="text-base w-[80%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center relative overflow-hidden transition-transform duration-300 ease-in-out 
                         hover:shadow-lg hover:shadow-stone-500/100 bg-stone-100 hover:bg-stone-800 text-stone-800 hover:text-stone-100 transform 
-                        hover:-translate-x-1 hover:-translate-y-1" onClick={() => {makeBooking(rid, bookDate)}}>Reserve Now!</button>
-                :
-                <Link href="/login">
-                <button className="text-base w-[80%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center relative overflow-hidden transition-transform duration-300 ease-in-out 
+                        hover:-translate-x-1 hover:-translate-y-1" onClick={() => { makeBooking(rid, bookDate) }}>Reserve Now!</button>
+                    :
+                    <Link href="/login">
+                        <button className="text-base w-[80%] mb-4 mr-4 inline-block border border-stone-800 p-2 text-center relative overflow-hidden transition-transform duration-300 ease-in-out 
                         hover:shadow-lg hover:shadow-stone-500/100 bg-stone-100 hover:bg-stone-800 text-stone-800 hover:text-stone-100 transform 
-                        hover:-translate-x-1 hover:-translate-y-1" onClick={() => {}}>Reserve Now!</button>
-                </Link>
+                        hover:-translate-x-1 hover:-translate-y-1" onClick={() => { }}>Reserve Now!</button>
+                    </Link>
                 }
             </div>
         </main>
