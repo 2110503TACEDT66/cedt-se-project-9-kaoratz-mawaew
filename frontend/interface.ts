8b5d614b3b9dcc3455db2b73ae1a4951bee7acc0
unknown <jasonchawsuan@gmail.com>
2024-04-20 15:28:22 +0700

    remove unused

--- frontend/interface.ts
+++ frontend/interface.ts
@@ -1,5 +1,3 @@
-import { Dayjs } from "dayjs";
-
 export interface UserItem{
     _id: string,
     name: string,
--- frontend/src/app/reserve/serverAction.tsx
+++
@@ -1,8 +0,0 @@
-'use server'
-
-import getRestaurant from "@/libs/getRestaurant"
-
-export async function haha ({setResData, rid} : {setResData : Function, rid: string}) {
-    const data = await getRestaurant(rid);
-    return setResData(data);
-}
