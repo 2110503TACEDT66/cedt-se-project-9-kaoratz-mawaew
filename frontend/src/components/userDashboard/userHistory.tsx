44d3cfe85835a326323e3f56f9cec34d400b2eda
unknown <jasonchawsuan@gmail.com>
2024-04-20 22:27:21 +0700

    User History #4

--- frontend/src/components/userDashboard/userHistory.tsx
+++ frontend/src/components/userDashboard/userHistory.tsx
@@ -7,12 +7,12 @@ export default function UserHistory ({reservation} : {reservation : reserveJson}
     return (
         
         <div className="w-full pl-[10%]">
-            <table className="text-left text-xs w-full items-center">
+            <table className="text-center text-sm w-full items-center">
                 <thead>
                     <tr>
-                        <th className="w-[15%] py-4 font-semibold">Date of Issue</th>
-                        <th className="w-[40%] py-4 font-semibold">Restaurant</th>
                         <th className="w-[15%] py-4 font-semibold">Date of Reservation</th>
+                        <th className="w-[40%] py-4 font-semibold">Restaurant</th>
+                        <th className="w-[15%] py-4 font-semibold">Date of Issue</th>
                         <th className="w-[10%] py-4 font-semibold">Status</th>
                         <th className="w-[10%] py-4 font-semibold text-center">Review</th>
                     </tr>
@@ -22,26 +22,31 @@ export default function UserHistory ({reservation} : {reservation : reserveJson}
                 data? data.map((res) => 
                     <tr>
                         <td className="w-[15%] py-4" suppressHydrationWarning>
-                            {dayjs(res.createdAt).format('DD/MM/YY')}
+                            {dayjs(res.resvDate).format('DD/MM/YY')}
                         </td>
                         <td className="w-[40%] py-4">
                             {res.restaurant.name}
                         </td>
                         <td className="w-[15%] py-4" suppressHydrationWarning>
-                            {dayjs(res.resvDate).format('DD/MM/YY')}
+                            {dayjs(res.createdAt).format('DD/MM/YY')}
                         </td> 
                         <td className="w-[10%] py-4">
-                            {res.completed? "Completed" : "Upcoming"}
+                            {res.completed? <p className="text-emerald-600">Completed</p> : <p className="text-red-600">Upcoming</p>}
                         </td>
-                        <td className="w-[10%] py-4 text-center">
+                        {
+                            res.completed? <td className="w-[10%] py-4">
                             <Link href={`/restaurant/${res.restaurant._id}`}>
-                                <button className="w-[81px] h-[37px] border border-stone-800 relative overflow-hidden transition-transform duration-300 ease-in-out 
+                                <button className="w-[55px] h-[25px] border border-stone-800 relative overflow-hidden transition-transform duration-300 ease-in-out 
                         hover:shadow-lg hover:shadow-stone-500/100 bg-stone-100 hover:bg-stone-800 text-stone-800 hover:text-stone-100 transform 
-                        hover:-translate-x-1 hover:-translate-y-1">
+                        hover:-translate-x-1 hover:-translate-y-1 text-xs">
                                     Review
                                 </button>
                             </Link>
-                        </td>
+                            </td> : <td className= "w-[10%] py-4">
+                                -
+                            </td>
+                        }
+                        
                     </tr>) : 
                 <tr>
                     <td className="w-[15%] py-4">
