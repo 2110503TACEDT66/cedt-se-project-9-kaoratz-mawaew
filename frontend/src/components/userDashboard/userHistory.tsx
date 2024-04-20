463cd188fbca716fd4bff0eef6db5eebd9027af7
unknown <jasonchawsuan@gmail.com>
2024-04-20 23:25:09 +0700

    User History #7 (border)

--- frontend/src/components/userDashboard/userHistory.tsx
+++ frontend/src/components/userDashboard/userHistory.tsx
@@ -7,13 +7,13 @@ export default function UserHistory ({reservation} : {reservation : reserveJson}
     return (
         
         <div className="w-full pl-[10%]">
-            <table className="text-center text-sm w-full items-center">
+            <table className="text-center text-sm w-full items-center border-b-2 border-gray-900">
                 <thead>
                     <tr>
-                        <th className="w-[15%] py-4 font-semibold">Date of Reservation</th>
-                        <th className="w-[40%] py-4 font-semibold">Restaurant</th>
-                        <th className="w-[15%] py-4 font-semibold">Date of Issue</th>
-                        <th className="w-[10%] py-4 font-semibold">Status</th>
+                        <th className="w-[15%] py-4 font-semibold border-r-2 border-gray-900">Date of Reservation</th>
+                        <th className="w-[40%] py-4 font-semibold border-r-2 border-gray-900">Restaurant</th>
+                        <th className="w-[15%] py-4 font-semibold border-r-2 border-gray-900">Date of Issue</th>
+                        <th className="w-[10%] py-4 font-semibold border-r-2 border-gray-900">Status</th>
                         <th className="w-[10%] py-4 font-semibold text-center">Review</th>
                     </tr>
                 </thead>
@@ -33,16 +33,16 @@ export default function UserHistory ({reservation} : {reservation : reserveJson}
             {
                 data.length > 0 ? data.map((res) => 
                     <tr>
-                        <td className="w-[15%] py-4" suppressHydrationWarning>
+                        <td className="w-[15%] py-4 border-r-2 border-gray-900" suppressHydrationWarning>
                             {dayjs(res.resvDate).format('DD/MM/YY')}
                         </td>
-                        <td className="w-[40%] py-4">
+                        <td className="w-[40%] py-4 border-r-2 border-gray-900">
                             {res.restaurant.name}
                         </td>
-                        <td className="w-[15%] py-4" suppressHydrationWarning>
+                        <td className="w-[15%] py-4 border-r-2 border-gray-900" suppressHydrationWarning>
                             {dayjs(res.createdAt).format('DD/MM/YY')}
                         </td> 
-                        <td className="w-[10%] py-4">
+                        <td className="w-[10%] py-4 border-r-2 border-gray-900">
                             {res.completed? <p className="text-emerald-600">Completed</p> : <p className="text-red-600">Upcoming</p>}
                         </td>
                         {
@@ -61,16 +61,16 @@ export default function UserHistory ({reservation} : {reservation : reserveJson}
                         
                     </tr>) : 
                 <tr>
-                    <td className="w-[15%] py-4">
+                    <td className="w-[15%] py-4 border-r-2 border-gray-900">
                         No History
                     </td>
-                    <td className="w-[40%] py-4">
+                    <td className="w-[40%] py-4 border-r-2 border-gray-900">
                         No History
                     </td>
-                    <td className="w-[15%] py-4">
+                    <td className="w-[15%] py-4 border-r-2 border-gray-900">
                         No History
                     </td>
-                    <td className="w-[10%] py-4">
+                    <td className="w-[10%] py-4 border-r-2 border-gray-900">
                         No History
                     </td>
                     <td className="w-[10%] py-4">
