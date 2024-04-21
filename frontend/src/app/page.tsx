f2d80af0b0a5d734ee0dc2216c2414b22b70a227
Auiz <nopphawit47374@gmail.com>
2024-04-21 16:43:08 +0700

    add login to dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,32 @@
 // pages/index.tsx
 
 import MainPageMiddle from "@/components/mainpage/herosection";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/components/auth";
+import getUserProfile from "@/libs/getUserProfile";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    const profile = await getUserProfile(session?.user.token)
+    login(profile.data.role)
+  }
+
   return (
     <main className=" w-[88%]">
       <MainPageMiddle />
     </main>
   );
+}
+
+const login = (role: string) => {
+  if (role == 'manager' || role == 'admin') {
+    redirect('/dashboard')
+  }
+
+  if (role == 'user') {
+    redirect('/reserve')
+  }
 }
