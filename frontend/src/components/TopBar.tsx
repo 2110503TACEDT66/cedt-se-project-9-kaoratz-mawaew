98e09fc38f518c98ea481cae5ff4491475f44eb3
unknown <jasonchawsuan@gmail.com>
2024-04-30 19:58:49 +0700

    dueng dueng

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -13,7 +13,9 @@ export default function TopBar({
       <div className="w-[85%] mr-4 flex items-center bg-primary">
         <Marquee />
       </div>
-      <div className="w-[15%] flex items-center z-20 bg-primary">
+      <div className="w-[15%] flex items-center z-20 bg-primary overflow-hidden transition-transform duration-300 ease-in-out 
+                        hover:shadow-lg hover:shadow-stone-500/100 hover:bg-stone-800 hover:text-stone-100 transform 
+                        hover:-translate-x-1 hover:-translate-y-1">
         <Link href="/dashboard">
           <p className="pl-2 pr-2 pt-1 pb-1 text-stone-100">
             <span className="hidden sm:inline">{`${userName} ${role}`}</span>
