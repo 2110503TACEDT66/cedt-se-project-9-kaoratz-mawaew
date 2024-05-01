8084b713030a7462f8b4166228eedc9bb86c47f4
Pawat <6633182821@student.chula.ac.th>
2024-05-01 11:31:24 +0700

    kjdfsalkaf;dshdsa

--- frontend/src/components/dashboard/Chart.tsx
+++ frontend/src/components/dashboard/Chart.tsx
@@ -13,27 +13,27 @@ interface ChartInput {
 
 export default function ChartComponent({ chartInput }: { chartInput: ChartInput[]}) {
 
-    const customTooltip = (props) => {
-        const { payload, active } = props;
-        if (!active || !payload) return null;
-        return (
-          <div className="w-56 rounded-tremor-default border border-tremor-border bg-tremor-background p-2 text-tremor-default shadow-tremor-dropdown">
-            {payload.map((category, idx) => (
-              <div key={idx} className="flex flex-1 space-x-2.5">
-                <div
-                  className={`flex w-1 flex-col bg-${category.color}-500 rounded`}
-                />
-                <div className="space-y-1">
-                  <p className="text-tremor-content">{category.dataKey}</p>
-                  <p className="font-medium text-tremor-content-emphasis">
-                    {category.value} hours 
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        );
-    }; 
+    // const customTooltip = (props) => {
+    //     const { payload, active } = props;
+    //     if (!active || !payload) return null;
+    //     return (
+    //       <div className="w-56 rounded-tremor-default border border-tremor-border bg-tremor-background p-2 text-tremor-default shadow-tremor-dropdown">
+    //         {payload.map((category, idx) => (
+    //           <div key={idx} className="flex flex-1 space-x-2.5">
+    //             <div
+    //               className={`flex w-1 flex-col bg-${category.color}-500 rounded`}
+    //             />
+    //             <div className="space-y-1">
+    //               <p className="text-tremor-content">{category.dataKey}</p>
+    //               <p className="font-medium text-tremor-content-emphasis">
+    //                 {category.value} hours 
+    //               </p>
+    //             </div>
+    //           </div>
+    //         ))}
+    //       </div>
+    //     );
+    // }; 
 
     const [value, setValue] = useState<EventProps>(null);
     return (
@@ -52,7 +52,7 @@ export default function ChartComponent({ chartInput }: { chartInput: ChartInput[
                 showTooltip={true}
                 animationDuration={1000}
                 showYAxis={true}
-                customTooltip={customTooltip}
+                // customTooltip={customTooltip}
             />
         </Card>
     );
---
+++ frontend/src/components/dashboard/Stat.tsx
@@ -0,0 +1,75 @@
+'use client'
+import dayjs from "dayjs";
+import { UserItem, reserveJson } from "../../../interface";
+import { getSession } from "next-auth/react";
+import { authOptions } from "../auth";
+import { getServerSession } from "next-auth";
+import getUserProfile from "@/libs/getUserProfile";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const formatNumber = (number: number) => {
+    return number < 10 ? `0${number}` : number;
+};
+
+export default function Statistic({reservation} : {reservation: reserveJson}){
+    const data = reservation.data;
+    const [current, setCurrent] = useState(0);
+    const [reservedSinceLastYear, setReservedSinceLastYear] = useState(0);
+    const [alltime, setAlltime] = useState(0);
+
+    // console.log(data)
+
+    
+    useEffect(() => {
+        let count = 0;
+        let inyear = 0;
+        let alltime = 0;
+        const currentyear = dayjs().year();
+
+        data.forEach((res) => {
+            
+            alltime++;
+            if (!res.completed) {
+                count++;
+            }
+            if (dayjs(res.resvDate, 'YYYY-MM-DDTHH:mm:ss').year() === currentyear){
+                inyear++;
+            }
+            
+        });
+        setCurrent(count);
+        setReservedSinceLastYear(inyear);
+        setAlltime(alltime);
+    }, [data]);
+
+    return(
+        <table className="mt-7">
+            <tbody>
+                <tr>
+                    <td className="pr-9 text-base border-r-2 border-black font-mono">
+                        <p>Current reservation</p>
+                        <div className="inline-flex items-center gap-4 mt-4">
+                            <h1 className="text-4xl font-bold">{formatNumber(current)}</h1>
+                            <p>reservation</p>
+                        </div>
+                    </td>
+                    <td className="px-9 text-base border-r-2 border-black font-mono">
+                        <p>In this year</p>
+                        <div className="inline-flex items-center gap-4 mt-4">
+                            <h1 className="text-4xl font-bold">{formatNumber(reservedSinceLastYear)}</h1>
+                            <p>reservation</p>
+                        </div>
+                    </td>
+                    <td className="pl-9 text-base font-mono">
+                        <p>For all time</p>
+                        <div className="inline-flex items-center gap-4 mt-4">
+                            <h1 className="text-4xl font-bold">{formatNumber(alltime)}</h1>
+                            <p>reservation</p>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
